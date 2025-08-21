f22bd5e4f585085657c2f6aa85e96beb9177a2f1
Diegov <davillatoro@email>
2025-08-21 13:18:15 -0600

    backend actualizacion 2

--- config/database.js
+++ config/database.js
@@ -5,6 +5,15 @@ const pool = mysql2.createPool({
   user: 'uqs7pyne3przlub1',
   password: 'b86T7mnED2glWjByiTws',
   database: 'bjyazuug6c5oskuh7jwy',
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+  dateStrings: true,
+    // Si Clever Cloud requiere SSL:
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool.promise();
